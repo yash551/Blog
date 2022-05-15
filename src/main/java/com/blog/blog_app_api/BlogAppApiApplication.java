5e39ae914b2d6d9a600374e14a497675a92777ad
Yash Gadhiya <yashgadhiya551@gmail.com>
2022-05-15 18:46:56 -0400

    initial commit

---
+++ src/main/java/com/blog/blog_app_api/BlogAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blog_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/blog_app_api/BlogAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blog_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
